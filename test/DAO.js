0d7883b8a99d6f3ccc6681a37b618f83f2c265e0
Borodinyar <Borodin.yaro@yandex.ru>
2022-10-27 23:55:47 +0400

    Created a project template in Hardhat

---
+++ test/DAO.js
@@ -0,0 +1,5 @@
+const { expect } = require("chai");
+
+describe("DAO contract", function () {
+  // TODO
+});
---
+++ test/Token.js
@@ -0,0 +1,5 @@
+const { expect } = require("chai");
+
+describe("Token contract", function () {
+  // TODO
+});
